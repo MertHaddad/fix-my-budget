524a8dd9dd6940a51d6bdaf262c6e6fe5b9b2827
MertHaddad <MertHaddad@users.noreply.github.com>
2024-01-21 18:36:35 +0000

    deploy: 21c1b795d969f789e6633e382a0a9383333b5bd0

--- server/src/middlewares/global-error-handler.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction,Request,Response } from 'express';
-import logger from '../config/logger';
-
-const globalErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  logger.error(err);
-  res.status(500).send('Internal Server Error');
-};
-
-export default globalErrorHandler;
--- server/src/services/auth-service.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosResponse } from 'axios';
-
-const fetchSearchResults = async (): Promise<AxiosResponse<any>> => {
-  const response = await axios.get(`${process.env.wikiBaseURL}`);
-  console.log(response.data);
-  return response.data;
-};
-
-export { fetchSearchResults };
