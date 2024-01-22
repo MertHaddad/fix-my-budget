6d743bee1b8297a85aabfe0d24fc1b77dce991e5
Mert Haddad <82902696+MertHaddad@users.noreply.github.com>
2024-01-22 11:23:36 +0300

    update next config

--- interface/next.config.mjs
+++ interface/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/fix-my-budget', // replace 'repo-name' with the name of your repository
+    assetPrefix: '/fix-my-budget/', // replace 'repo-name' with the name of your repository
+  
 };
 
 export default nextConfig;
