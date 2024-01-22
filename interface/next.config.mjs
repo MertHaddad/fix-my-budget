7350ecca50a697d95349807887765dae2d6de6d1
Mert Haddad <82902696+MertHaddad@users.noreply.github.com>
2024-01-22 11:33:21 +0300

    fix : next/image

--- interface/next.config.mjs
+++ interface/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/fix-my-budget', // replace 'repo-name' with the name of your repository
-    assetPrefix: '/fix-my-budget/', // replace 'repo-name' with the name of your repository
-  
+    basePath: '/fix-my-budget',
+    assetPrefix: '/fix-my-budget/', 
 };
 
 export default nextConfig;
